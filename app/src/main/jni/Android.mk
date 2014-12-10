3273b4256074360fe121889b9998a047bf66c093
Jaroslav O <jaroslav@ozevic.com>
2014-12-10 11:46:50 +0200

    v 1.0

---
+++ app/src/main/jni/Android.mk
@@ -0,0 +1,33 @@
+LOCAL_PATH := $(call my-dir)
+
+# Edit this line
+GSTREAMER_ROOT_ANDROID := /path/to/library/gstreamer-1.0-android-arm-release-1.4.4
+
+
+SHELL := PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin /bin/bash
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE    := tutorial-5
+LOCAL_SRC_FILES := tutorial-5.c
+LOCAL_SHARED_LIBRARIES := gstreamer_android
+LOCAL_LDLIBS := -llog -landroid
+include $(BUILD_SHARED_LIBRARY)
+
+
+
+ifndef GSTREAMER_ROOT
+ifndef GSTREAMER_ROOT_ANDROID
+$(error GSTREAMER_ROOT_ANDROID is not defined!)
+endif
+GSTREAMER_ROOT        := $(GSTREAMER_ROOT_ANDROID)
+endif
+GSTREAMER_NDK_BUILD_PATH  := $(GSTREAMER_ROOT)/share/gst-android/ndk-build
+
+
+include $(GSTREAMER_NDK_BUILD_PATH)/plugins.mk
+GSTREAMER_PLUGINS         := $(GSTREAMER_PLUGINS_CORE) $(GSTREAMER_PLUGINS_PLAYBACK) $(GSTREAMER_PLUGINS_CODECS) $(GSTREAMER_PLUGINS_NET) $(GSTREAMER_PLUGINS_SYS) $(GSTREAMER_PLUGINS_CODECS_RESTRICTED)
+G_IO_MODULES              := gnutls
+GSTREAMER_EXTRA_DEPS      := gstreamer-video-1.0
+
+include $(GSTREAMER_NDK_BUILD_PATH)/gstreamer-1.0.mk
---
+++ readme.md
@@ -0,0 +1,19 @@
+# GStreamer 1.0 example "tutorial 5" for Android Studio with Gradle
+
+Here is working tutorial-5 example. Code originally came from [Sebastian Dröge (slomo) blog](https://coaxion.net).
+To run this code on Android Studio, you have to make Gradle compile NDK code using .mk files
+originally made for Eclipse.
+
+Gradle code is made mainly thanks to [Gaku Uead's blog post](http://blog.gaku.net/ndk/). For some
+more details look at `app/build.gradle`.
+
+## How to
+
+1. Open project with Android Studio (tested with 1.0 version)
+2. Edit `local.properties` - set SDK and NDK paths
+3. Download GStreamer Android library from [freedesktop page](http://gstreamer.freedesktop.org/data/pkg/android/).
+ I was using [1.4.4 version from 08-Nov-2014 10:03](http://gstreamer.freedesktop.org/data/pkg/android/1.4.4/)
+ (gstreamer-1.0-android-arm-release-1.4.4.tar.bz2). Extract everything somewhere.
+4. Edit `src/main/jni/Android.mk` file and set `GSTREAMER_ROOT_ANDROID` to the path
+where your extracted Gstreamer library (step 3).
+5. Run
